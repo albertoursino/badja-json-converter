ad828eab19794a41d3148871ec8e63859415e9cd
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2020-12-10 23:09:54 +0100

    Reformatted code & Added unittests

--- code/write.py
+++ code/write.py
@@ -2,89 +2,59 @@ import glob
 import json
 
 import cv2
-import numpy as np
 import h5py
+import numpy as np
 import tqdm
-import pandas as pd
 
 IMAGE_SIZE = (512, 256)
 SKELETON_SIZE = 20
-IMG_IDX = 0
 HOME = 'C:/Users/Alberto Ursino/Desktop/IntellIj Local Files/Convert-BADJA-json/'
-
-idx_dict = {}
-
-hf = h5py.File(HOME + 'datasets/annotation_set_{}_{}.h5'.format(IMAGE_SIZE[0], IMAGE_SIZE[1]), 'w')
-skeleton_hf = h5py.File(HOME + 'skeleton.h5', 'r')
 
 
 def replace_slash(path):
     return path.replace('\\', '/')
 
 
-# Creating 'skeleton' dataset
+def images_sampler(url_list):
+    frames = []
+    img_idx = 0
+    dict_img = {}
+    for url in url_list:
+        for image_file in tqdm.tqdm(glob.glob(url)):
+            dict_img[replace_slash(image_file)] = img_idx
+            img_idx += 1
+            img = cv2.imread(image_file)
+            img = cv2.resize(img, IMAGE_SIZE)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            frames.append(img)
+    return frames, dict_img, img_idx
 
-hf.create_dataset('skeleton', data=skeleton_hf['skeleton'])
 
-# Creating 'images' dataset
-
-sampled_frames = []
-num_images = 0
-
-for image_file in tqdm.tqdm(
-        glob.glob(HOME + 'extra_videos/rs_dog/segmentations/*.png')):
-    idx_dict[replace_slash(image_file)] = IMG_IDX
-    IMG_IDX += 1
-    num_images += 1
-    img = cv2.imread(image_file)
-    img = cv2.resize(img, IMAGE_SIZE)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sampled_frames.append(img)
-
-for image_file in tqdm.tqdm(
-        glob.glob(HOME + 'DAVIS/Annotations/Full-Resolution/dog/*.png')):
-    idx_dict[replace_slash(image_file)] = IMG_IDX
-    IMG_IDX += 1
-    num_images += 1
-    img = cv2.imread(image_file)
-    img = cv2.resize(img, IMAGE_SIZE)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sampled_frames.append(img)
-
-for image_file in tqdm.tqdm(
-        glob.glob(HOME + 'DAVIS/Annotations/Full-Resolution/dog-agility/*.png')):
-    idx_dict[replace_slash(image_file)] = IMG_IDX
-    IMG_IDX += 1
-    num_images += 1
-    img = cv2.imread(image_file)
-    img = cv2.resize(img, IMAGE_SIZE)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sampled_frames.append(img)
-
-sampled_frames = np.concatenate(sampled_frames)
-sampled_frames = np.reshape(sampled_frames, (num_images, IMAGE_SIZE[1], IMAGE_SIZE[0], 1))
-
-hf.create_dataset('images', data=sampled_frames)
-print('images dataset created')
-
-# Creating 'annotations' and 'annotated' datasets
-
-none_matrix = np.zeros((num_images, SKELETON_SIZE, 2))
-for i in range(num_images):
-    for j in range(SKELETON_SIZE):
-        none_matrix[i][j] = np.nan
-
-hf.create_dataset('annotations', dtype=np.float64, data=none_matrix)
-hf.create_dataset('annotated', shape=(num_images, SKELETON_SIZE), dtype=bool)
+def delete_useless_joints(xy_list):
+    new_xy_list = []
+    for i in range(8, 11):
+        new_xy_list.append(xy_list[i])
+    for i in range(12, 16):
+        new_xy_list.append(xy_list[i])
+    for i in range(18, 21):
+        new_xy_list.append(xy_list[i])
+    for i in range(22, 26):
+        new_xy_list.append(xy_list[i])
+    for i in range(28, 29):
+        new_xy_list.append(xy_list[i])
+    for i in range(31, 34):
+        new_xy_list.append(xy_list[i])
+    for i in range(35, 37):
+        new_xy_list.append(xy_list[i])
+    return new_xy_list
 
 
 def swap_coordinates(xy_list):
-    swapped_xy_list = xy_list
     for i in range(len(xy_list)):
-        x = swapped_xy_list[i][0]
-        swapped_xy_list[i][0] = swapped_xy_list[i][1]
-        swapped_xy_list[i][1] = x
-    return swapped_xy_list
+        x = xy_list[i][0]
+        xy_list[i][0] = xy_list[i][1]
+        xy_list[i][1] = x
+    return xy_list
 
 
 def resize_coordinates(xy_list, rx, ry):
@@ -94,25 +64,6 @@ def resize_coordinates(xy_list, rx, ry):
     return resized_xy_list
 
 
-def delete_useless_joints(xy_list):
-    pruned_xy_list = []
-    for i in range(8, 11):
-        pruned_xy_list.append(xy_list[i])
-    for i in range(12, 16):
-        pruned_xy_list.append(xy_list[i])
-    for i in range(18, 21):
-        pruned_xy_list.append(xy_list[i])
-    for i in range(22, 26):
-        pruned_xy_list.append(xy_list[i])
-    for i in range(28, 29):
-        pruned_xy_list.append(xy_list[i])
-    for i in range(31, 34):
-        pruned_xy_list.append(xy_list[i])
-    for i in range(35, 37):
-        pruned_xy_list.append(xy_list[i])
-    return pruned_xy_list
-
-
 def insert_nan(joints_list, skeleton_size, index):
     for idx in range(skeleton_size):
         if joints_list[index][idx][0] + joints_list[index][idx][1] == 0:
@@ -120,62 +71,65 @@ def insert_nan(joints_list, skeleton_size, index):
     return joints_list
 
 
-resize_ratio_x = IMAGE_SIZE[0] / 1280
-resize_ratio_y = IMAGE_SIZE[1] / 720
-joints_coordinates = []
-num_ann = 0
-j_file = open(HOME + 'rs_dog.json')
-j_data = json.load(j_file)
-for xy in j_data:
-    joints = delete_useless_joints(xy['joints'])
-    joints = swap_coordinates(joints)
-    joints = resize_coordinates(joints, resize_ratio_x, resize_ratio_y)
-    joints_coordinates.append(joints)
-    image_name = xy['segmentation_path']
-    image_index = idx_dict.get(HOME + image_name)
-    #joints_coordinates = insert_nan(joints_coordinates, SKELETON_SIZE, num_ann)
-    hf["annotations"][image_index] = joints_coordinates[num_ann]
-    hf["annotated"][image_index] = np.ones(SKELETON_SIZE, int)
-    num_ann += 1
+def fill_annotations_ds(original_res, badja_json_paths, index_dict, hf_file):
+    for json_p, res in zip(badja_json_paths, original_res):
+        resize_ratio_x = IMAGE_SIZE[0] / res[0]
+        resize_ratio_y = IMAGE_SIZE[1] / res[1]
+        joints_coordinates = []
+        num_ann = 0
+        j_file = open(json_p)
+        j_data = json.load(j_file)
+        for xy in j_data:
+            joints = delete_useless_joints(xy['joints'])
+            joints = swap_coordinates(joints)
+            joints = resize_coordinates(joints, resize_ratio_x, resize_ratio_y)
+            joints_coordinates.append(joints)
+            image_name = xy['segmentation_path']
+            image_index = index_dict.get(HOME + image_name)
+            joints_coordinates = insert_nan(joints_coordinates, SKELETON_SIZE, num_ann)
+            hf_file["annotations"][image_index] = joints_coordinates[num_ann]
+            hf_file["annotated"][image_index] = np.ones(SKELETON_SIZE, int)
+            num_ann += 1
+        j_file.close()
 
-resize_ratio_x = IMAGE_SIZE[0] / 1920
-resize_ratio_y = IMAGE_SIZE[1] / 1080
-joints_coordinates = []
-num_ann = 0
-j_file = open(HOME + 'dog.json')
-j_data = json.load(j_file)
-for xy in j_data:
-    joints = delete_useless_joints(xy['joints'])
-    joints = swap_coordinates(joints)
-    joints = resize_coordinates(joints, resize_ratio_x, resize_ratio_y)
-    joints_coordinates.append(joints)
-    image_name = xy['segmentation_path']
-    image_index = idx_dict.get(HOME + image_name)
-    #joints_coordinates = insert_nan(joints_coordinates, SKELETON_SIZE, num_ann)
-    hf["annotations"][image_index] = joints_coordinates[num_ann]
-    hf["annotated"][image_index] = np.ones(SKELETON_SIZE, int)
-    num_ann += 1
 
-resize_ratio_x = IMAGE_SIZE[0] / 1920
-resize_ratio_y = IMAGE_SIZE[1] / 1080
-joints_coordinates = []
-num_ann = 0
-j_file = open(HOME + 'dog_agility.json')
-j_data = json.load(j_file)
-for xy in j_data:
-    joints = delete_useless_joints(xy['joints'])
-    joints = swap_coordinates(joints)
-    joints = resize_coordinates(joints, resize_ratio_x, resize_ratio_y)
-    joints_coordinates.append(joints)
-    image_name = xy['segmentation_path']
-    image_index = idx_dict.get(HOME + image_name)
-    joints_coordinates = insert_nan(joints_coordinates, SKELETON_SIZE, num_ann)
-    hf["annotations"][image_index] = joints_coordinates[num_ann]
-    hf["annotated"][image_index] = np.ones(SKELETON_SIZE, int)
-    num_ann += 1
+if __name__ == '__main__':
 
-print('annotations dataset created')
-print('annotated dataset created')
+    hf = h5py.File(HOME + 'datasets/annotation_set_{}_{}.h5'.format(IMAGE_SIZE[0], IMAGE_SIZE[1]), 'w')
 
-hf.close()
-j_file.close()
+    # Creating 'skeleton' dataset
+
+    skeleton_hf = h5py.File(HOME + 'skeleton.h5', 'r')
+    hf.create_dataset('skeleton', data=skeleton_hf['skeleton'])
+
+    # Creating 'images' dataset
+
+    urls = [HOME + 'extra_videos/rs_dog/segmentations/*.png', HOME + 'DAVIS/Annotations/Full-Resolution/dog/*.png',
+            HOME + 'DAVIS/Annotations/Full-Resolution/dog-agility/*.png']
+
+    sampled_frames, idx_dict, num_images = images_sampler(urls)
+
+    sampled_frames = np.concatenate(sampled_frames)
+    sampled_frames = np.reshape(sampled_frames, (num_images, IMAGE_SIZE[1], IMAGE_SIZE[0], 1))
+
+    hf.create_dataset('images', data=sampled_frames)
+
+    print('images dataset created')
+
+    # Creating 'annotations' and 'annotated' datasets
+
+    none_matrix = np.zeros((num_images, SKELETON_SIZE, 2))
+    for i in range(num_images):
+        for j in range(SKELETON_SIZE):
+            none_matrix[i][j] = np.nan
+
+    hf.create_dataset('annotations', dtype=np.float64, data=none_matrix)
+    hf.create_dataset('annotated', shape=(num_images, SKELETON_SIZE), dtype=bool)
+
+    fill_annotations_ds([(1280, 720), (1920, 1080), (1920, 1080)],
+                        [HOME + 'rs_dog.json', HOME + 'dog.json', HOME + 'dog_agility.json'], idx_dict, hf)
+
+    print('annotations dataset created')
+    print('annotated dataset created')
+
+    hf.close()
